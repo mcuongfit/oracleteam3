ab18c1132ecb2fbaeeeef2912da4843f7431d928
mcuong.fit <mcuong.fit@gmail.com>
2019-05-09 00:08:05 +0700

    update thong ke

--- oraclenhom3/oraclenhom3/Controllers/HomeController.cs
+++ oraclenhom3/oraclenhom3/Controllers/HomeController.cs
@@ -19,10 +19,67 @@ namespace oraclenhom3.Controllers
 
         public ActionResult Index()
         {
+
             var CTT = db.CHITIETTRAMS.Include(t => t.TRAM).Where(c => c.DA == 2 && c.MO == 2 && c.YEAR == 2008 && c.TRAM.NUOC.MANUOC == "VM" && c.TRAM.TENTRAM == "BAN ME THUAT");
             return View(CTT.ToList());
         }
+        [HttpPost]
+        public ActionResult thongke(string manuoc, int nam)
+        {
+            int songay;
+            List<double> tongts = new List<double>();
+            List<double> tonglms = new List<double>();
+            List<double> tongass = new List<double>();
+            List<double> tongtmins = new List<double>();
+            List<double> tongtmaxs = new List<double>();
+            double tongt = 0, tonglm = 0, tongas = 0, tongmin = 0, tongmax = 0;
+            thongkeviewmodel model = new thongkeviewmodel();
+            model.Nam = nam;
+            var nuoc = db.CHITIETTRAMS.Where(c => c.TRAM.NUOC.MANUOC == manuoc).First();
+            model.tennuoc = nuoc.TRAM.NUOC.TENNUOC;
+            for (int j = 1; j < 13; j++)
+            {
+                var thang1 = db.CHITIETTRAMS.Where(c => c.MO == j && c.TRAM.NUOC.MANUOC == manuoc && c.YEAR == nam).ToList();
+                songay = thang1.Count();
+                for (int i = 0; i < songay; i++)
+                {
+                    tongt = tongt + Convert.ToDouble(thang1[i].NHIETDO);
+                    tonglm = tonglm + Convert.ToDouble(thang1[i].LUONGMUA);
+                    tongas = tongas + Convert.ToDouble(thang1[i].APSUAT);
+                    tongmin = tongmin + Convert.ToDouble(thang1[i].TMIN);
+                    tongmax = tongmax + Convert.ToDouble(thang1[i].TMAX);
 
+                }
+                tongt = (((tongt / songay) - 32) * 5) / 9;
+
+                tongt = Math.Round(tongt, 2);
+
+                tongts.Add(tongt);
+                tonglm = tonglm / songay;
+                tonglm = Math.Round(tonglm, 2);
+                tonglms.Add(tonglm);
+                tongas = tongas / songay;
+                tongas = Math.Round(tongas, 2);
+                tongass.Add(tongas);
+
+
+                tongmin = (((tongmin / songay) - 32) * 5) / 9;
+                tongmin = Math.Round(tongmin, 2);
+                tongtmins.Add(tongmin);
+
+                tongmax = (((tongmax / songay) - 32) * 5) / 9;
+                tongmax = Math.Round(tongmax, 2);
+                tongtmaxs.Add(tongmax);
+                model.nhietdo = tongts;
+                model.apsuat = tongass;
+                model.luongmua = tonglms;
+                model.tmin = tongtmins;
+                model.tmax = tongtmaxs;
+            }
+
+
+            return View(model);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ oraclenhom3/oraclenhom3/Models/thongkeviewmodel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace oraclenhom3.Models
+{
+    public class thongkeviewmodel
+    {
+        public int Nam { get; set; }
+        public string tennuoc { get; set; }
+        public List<double> nhietdo { get; set; }
+        public List<double> luongmua { get; set; }
+        public List<double> apsuat { get; set; }
+        public List<double> tmax { get; set; }
+        public List<double> tmin { get; set; }
+
+    }
+}
