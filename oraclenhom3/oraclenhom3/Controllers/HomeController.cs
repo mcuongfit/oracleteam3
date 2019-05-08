e00fbdd2f630fc430333d64d93ba5357c531a85b
mcuong.fit <mcuong.fit@gmail.com>
2019-05-08 17:11:13 +0700

    index

--- oraclenhom3/oraclenhom3/Controllers/HomeController.cs
+++ oraclenhom3/oraclenhom3/Controllers/HomeController.cs
@@ -1,16 +1,26 @@
-﻿using System;
+﻿using oraclenhom3.Models;
+using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-
+using oraclenhom3.Models;
 namespace oraclenhom3.Controllers
 {
     public class HomeController : Controller
     {
+
+
+        private contexts db = new contexts();
+
         public ActionResult Index()
         {
-            return View();
+            var CTT = db.CHITIETTRAMS.Include(t => t.TRAM).Where(c => c.DA == 2 && c.MO == 2 && c.YEAR == 2008 && c.TRAM.NUOC.MANUOC == "VM" && c.TRAM.TENTRAM == "BAN ME THUAT");
+            return View(CTT.ToList());
         }
 
         public ActionResult About()
